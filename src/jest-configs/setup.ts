07b450f8fbe89f4648bf91a75f49a9fc76d99a33
Andrew Harari <thedrew88@gmail.com>
2020-09-23 13:39:10 -0400

    jest

--- src/jest-configs/setup.ts
+++
@@ -1,9 +0,0 @@
-// src/jest-config/setup.ts
-import 'jest-preset-angular';
-import './globalMocks';
-
-Object.defineProperty(global, 'Promise', { writable: false, value: global.Promise });
-
-module.exports = {
-  setupFilesAfterEnv: ['./jest.setup.ts'],
-};
